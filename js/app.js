0737f15d2c60d200ba3728f995b6c6af49b2671c
Michael McMillan <mikemc26m@comcast.net>
2022-10-05 01:31:25 +0000

    new pages uploaded

---
+++ js/app.js
@@ -0,0 +1,127 @@
+'use strict';
+
+
+let usersName = prompt('What is your name?');
+console.log(usersName);
+alert('Welcome! ' + usersName);
+
+
+
+let beenToMidwest = prompt('Have you been to the Mid-West?').toLowerCase();
+console.log(beenToMidwest);
+beenToMidwest = beenToMidwest.toLowerCase();
+console.log(beenToMidwest);
+
+if (beenToMidwest === 'yes' || beenToMidwest === 'y') {
+  // console.log('Thats great! Me too!');
+  alert('Thats great Me too!');
+  // if (beenToMidwest !== 'yes' || beenToMidwest !=='no') {
+// alert('Try again')
+}
+
+else if (beenToMidwest === 'no' || beenToMidwest === 'n') {
+  // console.log('That is too bad, its a good place to visit');
+  alert('Thats too bad, it is a great place to visit');
+} else {
+  // console.log('You need to answer with a Yes or a No');
+  alert('You need to answer with a Yes or a No');
+}
+
+
+
+
+let beenToWest = prompt('Have you been to the West Coast?').toLowerCase();
+console.log(beenToWest);
+beenToWest = beenToMidwest.toLowerCase();
+console.log(beenToWest);
+
+if (beenToWest === 'yes' || beenToWest === 'y') {
+  // console.log('Thats great! Me too!');
+  alert('Thats great Me too!');
+  // if (beenToWest !== 'yes' || beenToWest !=='no') {
+// alert('Try again')
+}
+
+else if (beenToWest === 'no' || beenToWest === 'n') {
+  // console.log('That is too bad, its a good place to visit');
+  alert('Thats too bad, it is a great place to visit');
+}
+else {
+  // console.log('You need to answer with a Yes or a No');
+  alert('You need to answer with a Yes or a No');
+}
+
+
+
+
+
+let likeComps = prompt('Do you like computers?').toLowerCase();
+console.log(likeComps);
+likeComps = likeComps.toLowerCase();
+console.log(likeComps);
+
+if (likeComps === 'yes' || likeComps === 'y') {
+  // console.log('Thats great! Me too!');
+  alert('Thats great Me too!');
+  // if (likeComps !== 'yes' || likeComps !=='no') {
+// alert('Try again')
+}
+
+else if (likeComps === 'no' || likeComps === 'n') {
+  // console.log('That is too bad, computers are great');
+  alert('Thats too bad, computers are great');
+}
+else {
+  // console.log('You need to answer with a Yes or a No');
+  alert('You need to answer with a Yes or a No');
+}
+
+
+
+let sports = prompt('Do you like sports?').toLowerCase();
+console.log(likeComps);
+sports = sports.toLowerCase();
+console.log(sports);
+
+if (sports === 'yes' || sports === 'y') {
+  // console.log('Thats great! Me too!');
+  alert('Thats great Me too!');
+  // if (sports !== 'yes' || sports !=='no') {
+// alert('Try again')
+}
+
+else if (sports === 'no' || sports === 'n') {
+  // console.log('That is too bad, sports are great');
+  alert('Thats too bad, computers are great');
+}
+else {
+  // console.log('You need to answer with a Yes or a No');
+  alert('You need to answer with a Yes or a No');
+}
+
+
+
+
+
+
+
+let fishing = prompt('Do you like to fish?').toLowerCase();
+console.log(fishing);
+fishing = fishing.toLowerCase();
+console.log(fishing);
+
+if (fishing === 'yes' || fishing === 'y') {
+  // console.log('Thats great! Me too!');
+  alert('Thats great Me too!');
+  // if (fishing !== 'yes' || fishing !=='no') {
+// alert('Try again')
+}
+
+else if (fishing === 'no' || fishing === 'n') {
+  // console.log('That is too bad, fishing is great');
+  alert('Thats too bad, fishing is great');
+}
+else {
+  // console.log('You need to answer with a Yes or a No');
+  alert('You need to answer with a Yes or a No');
+}
