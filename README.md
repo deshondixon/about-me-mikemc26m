9f44727fc69286180aab50c84b6f81ff95e49fcd
deshondixon <deshondixon@gmail.com>
2022-10-06 15:04:39 -0700

    deshon changes

--- README.md
+++ README.md
@@ -1,19 +1,23 @@
-# LABs - 2 & 3
+# LABs - 2 ,3, 4
 
 ## About Me - Lab 2
 
-The index page tells the user about me. I added JS that promted the user to guess yes or no to 5 questions.  
+The index page tells the user about me. I added JS that promoted the user to guess yes or no to 5 questions.  
 
 ## Lab 3
 
 I added JS to prompt the user to guess my favorite items and limited them on the number of guesses.
 
-### Author: Student/Group Name (Add name of any help I got)
+## Lab 4
+
+In this lab I cloned repositories and had my partner add functions.
+
+### Author: Mike McMillan/DeShon Dixon
 
 ### Links and Resources
 
-* [submission PR](http://xyz.com)
-* Any Links you used as reference
+* [submission PR](mikemc26m.github.io/about-me/)
+* Any Links you used as reference: (https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/proposing-changes-to-your-work-with-pull-requests/creating-a-pull-request)
 
 ### Reflections and Comments
 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-
+function usersName () {
 let usersName = prompt('What is your name?');
 console.log(usersName);
 alert('Welcome! ' + usersName);
@@ -8,10 +8,10 @@ alert('Welcome! ' + usersName);
 
 
 let score = 0;
+}
+usersName();
 
-
-
-
+function beenToMidwest() {
 
 let beenToMidwest = prompt('Have I been to the Mid-West?').toLowerCase();
 beenToMidwest = beenToMidwest.toLowerCase();
@@ -20,7 +20,6 @@ if (beenToMidwest === 'yes' || beenToMidwest === 'y') {
   // console.log('Thats great! Me too!');
   alert('You are correct! I grew up there.');
   if (beenToMidwest !== 'yes' || beenToMidwest !=='no');
-  alert('Try again');
 }
 
 else if (beenToMidwest === 'no' || beenToMidwest === 'n') {
@@ -31,25 +30,22 @@ else if (beenToMidwest === 'no' || beenToMidwest === 'n') {
   alert('You need to answer with a Yes or a No');
 }
 
+}
+beenToMidwest();
 
+//-------------------------------------------------------------------------------
 
-
-
-
-
-
+function beenToWest() {
 
 let beenToWest = prompt('Have I been to the West Coast?').toLowerCase();
 // console.log(beenToWest);
-beenToWest = beenToMidwest.toLowerCase();
+beenToWest = beenToWest.toLowerCase();
 // console.log(beenToWest);
 
 if (beenToWest === 'yes' || beenToWest === 'y') {
   // console.log('Thats great! Me too!');
   alert('Yup, been to all West Coast States.');
-}
-if (beenToWest !== 'yes' || beenToWest !=='no') {
-  alert('Try again');
+
 }
 
 else if (beenToWest === 'no' || beenToWest === 'n') {
@@ -61,14 +57,11 @@ else {
   // console.log('You need to answer with a Yes or a No');
   alert('You need to answer with a Yes or a No');
 }
+}
+beenToWest();
+//--------------------------------------------------------------------------
 
-
-
-
-
-
-
-
+function likeComps() {
 
 let likeComps = prompt('Do I like PC computers over Mac?').toLowerCase();
 // console.log(likeComps);
@@ -77,28 +70,22 @@ likeComps = likeComps.toLowerCase();
 
 if (likeComps === 'yes' || likeComps === 'y') {
   // console.log('You bet! Apples are for eating.');
-  alert('Nope, apples are for eating!');
-}
-if (likeComps !== 'yes' || likeComps !=='no') {
-  alert('Try again');
-}
-
-
-else if (likeComps === 'no' || likeComps === 'n') {
-  // console.log('That is too bad, computers are great');
   alert('Correct, apples are for eating.');
 }
+else if (likeComps === 'no' || likeComps === 'n') {
+  // console.log('That is too bad, computers are great');
+  alert('Nope, apples are for eating!');
+}
+
 else {
   // console.log('You need to answer with a Yes or a No');
   alert('You need to answer with a Yes or a No');
 }
+}
+likeComps();
+//---------------------------------------------------------------------
 
-
-
-
-
-
-
+function sports() {
 
 let sports = prompt('Do I like sports?').toLowerCase();
 // console.log(likeComps);
@@ -107,9 +94,7 @@ sports = sports.toLowerCase();
 
 if (sports === 'yes' || sports === 'y') {
   // console.log('Thats great! Me too!');
-  alert('Yeah, watching them more than playing, except for DISC GOLF, it\'s great!');}
-if (sports !== 'yes' || sports !=='no') {
-  alert('Try again');
+  alert('Yeah, watching them more than playing, except for DISC GOLF, it\'s great!');
 }
 
 
@@ -121,37 +106,37 @@ else {
   // console.log('You need to answer with a Yes or a No');
   alert('You need to answer with a Yes or a No');
 }
+}
+sports();
 
+//---------------------------------------------------------------------------
 
-
-
-
-
-
-
-
-
+function fishing() {
 let fishing = prompt('Do I like to fish?').toLowerCase();{
-// console.log(fishing);
-  fishing = fishing.toLowerCase();
-// console.log(fishing);
+  // console.log(fishing);
+    fishing = fishing.toLowerCase();
+  // console.log(fishing);
+  }
+  if (fishing === 'yes' || fishing === 'y') {
+    // console.log('Thats great! Me too!');
+    alert('Thats right! I love to crab and catch catfish and panfish!');
+    if (fishing !== 'yes' || fishing !=='no');
+    alert('Try again');
+  }
+  
+  else if (fishing === 'no' || fishing === 'n') {
+    // console.log('That is too bad, fishing is great');
+    alert('Wrong, fishing is great!');
+  }
+  else {
+    // console.log('You need to answer with a Yes or a No');
+    alert('You need to answer with a Yes or a No');
+  }
+  
 }
-if (fishing === 'yes' || fishing === 'y') {
-  // console.log('Thats great! Me too!');
-  alert('Thats right! I love to crab and catch catfish and panfish!');
-  if (fishing !== 'yes' || fishing !=='no');
-  alert('Try again');
-}
+fishing();
 
-else if (fishing === 'no' || fishing === 'n') {
-  // console.log('That is too bad, fishing is great');
-  alert('Wrong, fishing is great!');
-}
-else {
-  // console.log('You need to answer with a Yes or a No');
-  alert('You need to answer with a Yes or a No');
-}
-
+//-------------------------------------------------------------------------------
 
 alert ('Now lets play a game. You will need to guess a number between 1 and 10');
 
